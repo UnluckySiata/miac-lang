f4c6c13a0b37b2c6f1c04fc3391f5c31453e0252
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-01-04 13:45:01 +0100

    simple fibonacci seq

---
+++ examples/fib.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int fib(int x) {
+if (x == 0 || x == 1) {
+return 1;
+}
+return fib(x - 1) + fib(x - 2);
+}
+int main() {
+const int x = 5;
+int a = fib(x);
+const int q = printf("fib %d: %d\n", x, a);
+}
